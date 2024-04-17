4a548eef2e9f3df218f8610a0441feaba1514d69
EstyGlez <estibalizzmakeup@gmail.com>
2024-04-17 10:05:17 +0200

    changes

--- GaleriaColeccionistaBack/index.py
+++ GaleriaColeccionistaBack/index.py
@@ -1,9 +1,28 @@
 from src import init_app
 from config import config
+from flask_cors import CORS
 
 configuration = config['development']
 app= init_app(configuration)
 
+CORS(app)
+
 if __name__=='__main__':
 
     app.run()
+
+
+
+
+# from src import init_app
+# from config import config
+# from flask_cors import CORS
+
+# configuration = config['development']
+# app= init_app(configuration)
+
+# CORS(app)
+
+# if __name__=='__main__':
+
+#     app.run()
