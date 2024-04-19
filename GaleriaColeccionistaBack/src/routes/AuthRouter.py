e4d981ee72553343602baf1b4e91d398aa89c32d
sandra-medina-luque <153823146+sandra-medina-luque@users.noreply.github.com>
2024-04-19 13:25:56 +0200

    token

--- GaleriaColeccionistaBack/src/routes/AuthRouter.py
+++ GaleriaColeccionistaBack/src/routes/AuthRouter.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, request, jsonify
 from src.services.AuthService import AuthService
 from src.models.userModel import User
+from src.utils.Security import Security
+from src.models.personModel import Person
+
 
 main = Blueprint('login_blueprint_post', __name__)
 
@@ -11,22 +14,18 @@ def post_login_user():
 
     user_name = request.json ['user_name']
     password = request.json ['password']
-    
-    
 
-    print(user_name)
-    print(password)
-    
-    
+    user= (User(0,user_name,password,None))
+    person=(Person(None, None, None, None, None, None, None, None))
+    print(user)
 
-    user= User(0,user_name,password,None)
-
-
-    log_user= AuthService.login_user(user)
+    log_user= AuthService.login_user(user, person)
+    print('Hola')
     print(log_user)
 
     if ( log_user != None):
-        return jsonify({'success':True})
+        encode_token=Security.generate_token(log_user)
+        return jsonify({'success':True, 'token':encode_token})
     else:
         return jsonify({'success':False})
     
--- GaleriaColeccionistaBack/src/services/AuthService.py
+++ GaleriaColeccionistaBack/src/services/AuthService.py
@@ -1,35 +1,36 @@
 from src.database.db_mysql import get_connection
 from src.models.userModel import User
+from src.models.personModel import Person
 from werkzeug.security import check_password_hash
+
 class AuthService():
 
     @classmethod
-    def login_user(cls, user: User):
+    def login_user(cls, user: User, person:Person):
         try:
             connection = get_connection()
 
             authenticated_user = None
 
             with connection.cursor() as cursor:
-                # Imprime el nombre de usuario antes de llamar al procedimiento almacenado
-                print("Nombre de usuario:", user.user_name)
                 
-                cursor.execute('CALL sp_login_user(%s)', (user.user_name,))
+                cursor.execute('CALL sp_login_user(%s)', (user.user_name))
                 row = cursor.fetchone()
 
-                # Imprime la fila devuelta por el procedimiento almacenado
-                print("Fila obtenida:", row)
-                print("Row 2:", row[2])
-
                 if row is not None and check_password_hash(row[2], user.password):
-                    authenticated_user = row
+                    user= User(int(row[0]), row[1], row[2], row[3])
+    
+                    person=Person(row[4], row[5], None, None, None, None, None, user)
+                    authenticated_user = person
+                    print('A continuación este es el authenticated_user:')
+                    print(authenticated_user)
+
                 else:
                     authenticated_user = None 
 
             connection.close()
 
-            # Imprime el usuario autenticado (puede ser None si la autenticación falla)
-            print("Usuario autenticado:", authenticated_user)
+            
 
             return authenticated_user
 
