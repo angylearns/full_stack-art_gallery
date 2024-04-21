b722ad3dcd05b09c83176ef1864b21c8b14c95da
María <ary023ail.com>
2024-04-21 19:05:47 +0200

    .

--- GaleriaColeccionistaFront/src/App.jsx
+++ GaleriaColeccionistaFront/src/App.jsx
@@ -1,20 +1,12 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import Gallery from './components/gallery/Gallery'
-import ShoppingCart from './components/shoppingCart/ShoppingCart'
-import React from 'react';
-import EndPurchase from './components/endPurchase/EndPurchase';
-import ProductDetail from './components/productDetail/ProductDetail';
-import AdminMenu from './components/adminMenu/AdminMenu'
 
 function App() {
   
 
   return (
     <>
-      <ShoppingCart/>
-    {/* <ProductDetail /> */}
-    {/* <AdminMenu/> */}
+      
     </>
   )
 }
