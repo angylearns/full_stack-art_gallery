6bfc97e0b5c2a8e4f3b29c3b62f5dddc8559df8a
EstyGlez <estibalizzmakeup@gmail.com>
2024-04-22 21:18:48 +0200

    added music

--- GaleriaColeccionistaFront/src/components/lastProducts/LastProducts.jsx
+++ GaleriaColeccionistaFront/src/components/lastProducts/LastProducts.jsx
@@ -3,35 +3,38 @@ import productService from '../../services/productService';
 import './lastProducts.css';
 
 const LastProducts = () => {
-  const [recentProducts, setRecentProducts] = useState([]);
+ const [recentProducts, setRecentProducts] = useState([]);
 
-  useEffect(() => {
+ useEffect(() => {
     fetchRecentProducts();
-  }, []);
+ }, []);
 
-  const fetchRecentProducts = async () => {
+ const fetchRecentProducts = async () => {
     try {
       const productsData = await productService.getRecentProducts();
       setRecentProducts(productsData);
     } catch (error) {
       console.error('Error al obtener los últimos productos:', error);
     }
-  };
+ };
 
-  return (
+ return (
     <div className='product-gallery'>
       <h1>Obras recién llegadas</h1>
       <div className="product-list">
         {recentProducts.map((product) => (
           <div key={product.id} className="product-item">
-            <img src={product.url} alt={product.title} width={360}
-            height={420}/>
+            <img src={product.url} alt={product.title} width={360} height={420}/>
             <h3>{product.title}</h3>
+            <audio controls>
+              <source src="/song.mp3" type="audio/mpeg" />
+              Tu navegador no soporta el elemento de audio.
+            </audio>
           </div>
         ))}
       </div>
     </div>
-  );
+ );
 };
 
 export default LastProducts;
