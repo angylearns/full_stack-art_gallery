4e16bcc56db9b1cb61ff095e1ad5b3927508d893
sandra-medina-luque <153823146+sandra-medina-luque@users.noreply.github.com>
2024-04-24 00:06:48 +0200

    testing

--- GaleriaColeccionistaFront/tests/form.test.jsx
+++ GaleriaColeccionistaFront/tests/form.test.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import { render, fireEvent, waitFor, getByPlaceholderText, getByText } from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import Login from '../src/components/login/Login.jsx';
 import '@testing-library/jest-dom';
 
+// Simulamos el comportamiento de useNavigate
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => jest.fn(),
+}));
+
 describe('Login Component', () => {
   test('renders login form correctly', () => {
-    const { getByPlaceholderText, getByText, getByRole } = render(<Login isOpen={true} onClose={() => { }} onLogin={() => { }} />);
+    const { getByPlaceholderText, getByText } = render(
+      <Login isOpen={true} onClose={() => { }} onLogin={() => { }} />
+    );
+  
+    // Verifica si solo hay un elemento con el texto "Iniciar Sesión" (debe ser un h2)
+    expect(getByText("Iniciar Sesión", { selector: 'h2' })).toBeInTheDocument();
 
-    // Verifica si los elementos del formulario se renderizan correctamente
-    const loginTitleElement = getByText("Iniciar Sesión", { selector: 'h2[aria-label="login-title"]' });
-    expect(loginTitleElement).toBeInTheDocument();
     expect(getByPlaceholderText("Usuario")).toBeInTheDocument();
     expect(getByPlaceholderText("Contraseña")).toBeInTheDocument();
     expect(getByText("Tipo de usuario")).toBeInTheDocument();
-
-    // Encuentra el botón "Regístrate" por su rol
-    const registerButton = getByRole("button", { name: /Regístrate/i });
-    expect(registerButton).toBeInTheDocument();
+    expect(getByText("Regístrate")).toBeInTheDocument();
   });
 
   test('allows user to switch between login and register views', async () => {
-    const { getByPlaceholderText, getByText } = render(<Login isOpen={true} onClose={() => { }} onLogin={() => { }} />);
+    const { getByText, getByPlaceholderText } = render(
+      <Login isOpen={true} onClose={() => { }} onLogin={() => { }} />
+    );
 
     // Simula hacer clic en el botón "Regístrate"
     fireEvent.click(getByText(/Regístrate/i));
@@ -35,7 +42,5 @@ describe('Login Component', () => {
       expect(getByPlaceholderText("Correo Electrónico")).toBeInTheDocument();
       expect(getByPlaceholderText("Teléfono")).toBeInTheDocument();
     });
-
-    // Realiza más aserciones para la vista de registro si es necesario
   });
 });
