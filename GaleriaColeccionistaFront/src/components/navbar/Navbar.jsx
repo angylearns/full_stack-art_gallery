ee1f58b0e2a1abd95d1c0eced22bf47675f71032
María <ary023ail.com>
2024-04-23 23:29:15 +0200

    logout

--- GaleriaColeccionistaFront/src/components/navbar/Navbar.jsx
+++ GaleriaColeccionistaFront/src/components/navbar/Navbar.jsx
@@ -8,15 +8,17 @@ import home from "../../images/home.svg";
 import shoppingCart from "../../images/shoppingCart.svg";
 import line from "../../images/line.svg";
 import Login from '../login/Login';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 // import Swal from 'sweetalert2'
 import ShoppingCart from '../shoppingCart/ShoppingCart';
-
+import Cookies from 'js-cookie';
+import { useNavigate } from 'react-router-dom';
 
 function Navbar() {
 
   const [isLoginOpen, setIsLoginOpen] = useState(false); // Declaración del estado isLoginOpen
   const [mostrarComponente, setMostrarComponente] = useState(false);
+  const navigate = useNavigate();
 
   const handleOpenLogin = () => {
     console.log('Navbar.jsx handlepenlogin');
@@ -27,116 +29,130 @@ function Navbar() {
   // DESLOGUEARSE
   const handleLogout = () => {
     // Eliminar el usuario del almacenamiento local
-    localStorage.removeItem('user');
+    // localStorage.removeItem('user');
+    // // Limpiar el estado del usuario en el componente
+    // setUser(null);
+    // Swal.fire({
+    //   position: "center",
+    //   icon: "success",
+    //   title: "Ha cerrado sesión",
+    //   showConfirmButton: false,
+    //   timer: 1500
+    // });
+    // Eliminar el usuario de las cookies
+   
+    Cookies.remove('user_name');
+    Cookies.remove('first_name');
+    Cookies.remove('id_user');
+    Cookies.remove('id_person');
+    Cookies.remove('password');
+    Cookies.remove('user_type');
     // Limpiar el estado del usuario en el componente
     setUser(null);
-    Swal.fire({
-      position: "center",
-      icon: "success",
-      title: "Ha cerrado sesión",
-      showConfirmButton: false,
-      timer: 1500
-    });
-  };
+    navigate('/');
+    alert("vete ya a dormir ");
+    
 
-  useEffect(() => {
-    const loggedInUser = localStorage.getItem('user');
-    if (loggedInUser) {
-      const foundUser = JSON.parse(loggedInUser);
-      setUser(foundUser);
-    }
-  }, []);
+};
 
-  const handleLoginSuccess = (user) => {
-    console.log('handleLoginSuccess ' + JSON.stringify(user));
-    console.log('handleLoginSuccess ' + user);
-    if (user) {
-      setUser(JSON.parse(user)); // Actualiza el estado del usuario después de iniciar sesión
-    }
+useEffect(() => {
+  const loggedInUser = localStorage.getItem('user');
+  if (loggedInUser) {
+    const foundUser = JSON.parse(loggedInUser);
+    setUser(foundUser);
+  }
+}, []);
 
-  };
+const handleLoginSuccess = (user) => {
+  console.log('handleLoginSuccess ' + JSON.stringify(user));
+  console.log('handleLoginSuccess ' + user);
+  if (user) {
+    setUser(JSON.parse(user)); // Actualiza el estado del usuario después de iniciar sesión
+  }
 
-  const handleClick = () => {
-    // Cambia el estado para mostrar el componente
-    setMostrarComponente(true);
-  };
+};
 
-  const handleCerrarComponente = () => {
-    // Cambia el estado para ocultar el componente
-    setMostrarComponente(false);
+const handleClick = () => {
+  // Cambia el estado para mostrar el componente
+  setMostrarComponente(true);
+};
 
-    // Navegar hacia atrás en el historial del navegador
-    window.history.back();
-  };
+const handleCerrarComponente = () => {
+  // Cambia el estado para ocultar el componente
+  setMostrarComponente(false);
+
+  // Navegar hacia atrás en el historial del navegador
+  window.history.back();
+};
 
 
-  return (
-    <>
+return (
+  <>
 
-      <div className='container-navbar'>
+    <div className='container-navbar'>
 
-        <div className='icons-group'>
+      <div className='icons-group'>
 
-          <div className='logo-and-gallery-button'>
+        <div className='logo-and-gallery-button'>
 
-            <img src={logo} className='logo' alt="" />
+          <img src={logo} className='logo' alt="" />
 
-            <Link to="/Gallery">
-              <button className='gallery_button'>GALERIA</button>
-            </Link>
+          <Link to="/Gallery">
+            <button className='gallery_button'>GALERIA</button>
+          </Link>
 
-            <button className='home'>
-              <img src={home} alt="imghome" className='imghome' />
-            </button>
-
-          </div>
-
-
-          <p className='navbaruserhello'>{user ? `Hola, ${user.user_name}` : '¡Hola!'}</p>
-
-
-          <form className='navbar-form'>
-            <input className='input-navbar'
-              type="text"
-              // placeholder=""
-              value=''
-              onChange={(e) => setSearchValue(e.target.value)}
-            />
-          </form>
-
-          <img src={search} className='search' alt="" />
-
-
-          {/* PROPS */}
-
-          <button onClick={handleOpenLogin} className='login'>
-            <img src={login} alt="imglogin" className='imglogin' />
+          <button className='home'>
+            <img src={home} alt="imghome" className='imghome' />
           </button>
 
-          <button onClick={handleLogout} className='logout'>
-            <img src={logout} alt="imglogout" className='imglogout' />
-          </button>
-
-          <img src={shoppingCart} className='shoppingcart' alt="shoppingcart" onClick={handleClick} />
         </div>
 
 
-        <div className='linedivider'>
-          <img src={line} className='line' alt="line" />
-        </div>
+        <p className='navbaruserhello'>{user ? `Hola, ${user.user_name}` : '¡Hola!'}</p>
 
-        <div className='login_logout'>
-          <Login isOpen={isLoginOpen} onClose={() => setIsLoginOpen(false)} onLogin={handleLoginSuccess} />
-        </div>
-        <div >
-          {/* Renderiza el componente si mostrarComponente es true */}
-          {mostrarComponente && <ShoppingCart onClose={handleCerrarComponente}/>}
-        </div>
+
+        <form className='navbar-form'>
+          <input className='input-navbar'
+            type="text"
+            // placeholder=""
+            value=''
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+        </form>
+
+        <img src={search} className='search' alt="" />
+
+
+        {/* PROPS */}
+
+        <button onClick={handleOpenLogin} className='login'>
+          <img src={login} alt="imglogin" className='imglogin' />
+        </button>
+
+        <button onClick={handleLogout} className='logout'>
+          <img src={logout} alt="imglogout" className='imglogout' />
+        </button>
+
+        <img src={shoppingCart} className='shoppingcart' alt="shoppingcart" onClick={handleClick} />
       </div>
 
 
-    </>
-  )
+      <div className='linedivider'>
+        <img src={line} className='line' alt="line" />
+      </div>
+
+      <div className='login_logout'>
+        <Login isOpen={isLoginOpen} onClose={() => setIsLoginOpen(false)} onLogin={handleLoginSuccess} />
+      </div>
+      <div >
+        {/* Renderiza el componente si mostrarComponente es true */}
+        {mostrarComponente && <ShoppingCart onClose={handleCerrarComponente} />}
+      </div>
+    </div>
+
+
+  </>
+)
 }
 
 export default Navbar;
